1a9be206866c4c08d79f4113c518fd199a7cb6ed
Ryan Erricson <ryanerricson@gmail.com>
2021-05-12 14:46:20 -0400

    commit tree 1f881f0bc7022448d290b9aa6473be6eeeb42c1c

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+function saturdayFun(str = 'roller-skate'){
+    return `This Saturday, I want to ${str}!`
+}
+
+function mondayWork(str = 'go to the office'){
+    return `This Monday, I will ${str}.`
+}
+
+function wrapAdjective(wrap = '*') {
+    return function prompt(str = 'special'){
+        return `You are ${wrap}${str}${wrap}!`
+    }
+
+}
+
+const Calculator = {
+    add(a,b){
+        return a + b
+    },
+
+    subtract(a,b){
+        return a - b
+    },
+
+    multiply(a, b){
+        return a * b
+    }, 
+
+    divide(a,b){
+        return a / b
+    }
+}
+
+function actionApplyer(start, arr){
+    for (let i = 0; i < arr.length; i++){
+        start = arr[i](start);
+    }
+    return start
+}
